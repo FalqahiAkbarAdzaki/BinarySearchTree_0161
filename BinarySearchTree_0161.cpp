522917a2d68b80ef9cc9b916e604d30a90598e9f
AQA'S LAPTOP <AQA'S LAPTOP@LAPTOP-AA3N1G6J>
2024-06-13 10:46:08 +0700

    5

--- BinarySearchTree_0161.cpp
+++ BinarySearchTree_0161.cpp
@@ -7,4 +7,13 @@ class Node
 public:
 	string info;
 	Node* leftchild;
+	Node* rightchild;
+
+	//constructor for the node class
+	Node(string i, Node* l, Node* r)
+	{
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
 };
