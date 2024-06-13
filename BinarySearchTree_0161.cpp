29c3e03052889b41dabfcfdefcf616c7140eafc8
AQA'S LAPTOP <AQA'S LAPTOP@LAPTOP-AA3N1G6J>
2024-06-13 10:59:57 +0700

    15

--- BinarySearchTree_0161.cpp
+++ BinarySearchTree_0161.cpp
@@ -36,5 +36,8 @@ public:
 
 		Node* parent = nullptr;
 		Node* currentNode = nullptr;
+		search(element, parent, currentNode); // Locate the node which will be the parent of the node to be inserted
+
+		if (parent == nullptr) // If the parent is NULL (Tree is empty)
 	}
 };
