38bfc7ce66271611dd4f04dcd523372eb2f54860
AQA'S LAPTOP <AQA'S LAPTOP@LAPTOP-AA3N1G6J>
2024-06-13 11:46:58 +0700

    33

--- BinarySearchTree_0161.cpp
+++ BinarySearchTree_0161.cpp
@@ -133,28 +133,29 @@ int main()
 		switch (ch)
 		{
 			case '1';
-		{
-		    cout << "Enter a word: ";
-			string word;
-			cin >> word;
-			x.insert(word);
-			break;
-		}
-		case '2';
-		{				
-			x.inorder(x.ROOT);
-			break;
-		}
-		case '3';
-		{
+			{
+				cout << "Enter a word: ";
+				string word;
+				cin >> word;
+				x.insert(word);
+				break;
+			}
+			case '2';
+			{
+				x.inorder(x.ROOT);
+				break;
+			}
+			case '3';
+			{
 				x.preorder(x.ROOT);
 				break;
-		}
-		case '4';
-		{
+			}
+			case '4';
+			{
 				x.postorder(x.ROOT);
 				break;
+			}
+
 		}
-	
 	}
 }
